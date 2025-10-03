392691a9a72b53b76639069d9d843ff90e772c5e
Stevekim <131708216+Solsauti@users.noreply.github.com>
2025-10-03 12:37:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("John Doe", 2002, 2025, "S123", "Business IT");
+        s1.displayDetails();
+    }
+}
